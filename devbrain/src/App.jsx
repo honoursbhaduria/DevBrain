82884a542b2e8bf3b745fc29f1fde73378de6429
honoursbhaduria <honoursbhadauria@gmail.com>
2025-09-07 15:42:56 -0500

    fit : tailwind

--- devbrain/src/App.jsx
+++ devbrain/src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    <h1>hello fucker </h1>
-    </>
+    <div>App</div>
   )
 }
 
