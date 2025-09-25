2056526c451010723c0549ff88091346b56ebf76
honoursbhaduria <honoursbhadauria@gmail.com>
2025-09-24 21:55:48 -0500

    navbar added

--- devbrain/src/components/Navbar.jsx
+++ devbrain/src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='w-full px-20 py-8 '>
-        <div className='logo'>DEVBRAIN</div>
+    <div className='w-full px-20 py-8 flex justify-between items-center '>
+        <div className='logo text-xl font-bold '>DEVBRAIN</div>
         
-        <div className='links'>
-            {["Services" , "Our Me" , "About Services" ,"Insights" .map((item ,index )=> 
-            (
-                <a  key={index} className='text-md font-semibold'>{item}</a>
-            )
-            )]}
-        </div>
+        <div className='links flex gap-6'>
+        {["Services", "Our Me", "About Services", "Insights"].map((item, index) => (
+          <a key={index} href="/" className='text-md font-semibold hover:text-blue-500'>
+            {item}
+          </a>
+        ))}
+      </div>
         
         </div>
   )
