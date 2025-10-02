c68ae7a7ba8844d7925ece058a3c4de518fe7ec6
honoursbhaduria <honoursbhadauria@gmail.com>
2025-10-01 23:15:49 -0500

    content added

--- devbrain/src/components/Footer.jsx
+++ devbrain/src/components/Footer.jsx
@@ -4,10 +4,11 @@ const Footer = () => {
   return (
     <div className=' w-full h-screen  bg-zinc-900 py-20 px-20 '>
         <div className='w-1/2 '>
-             <h1>   eye opening </h1>
+             <h1 className='w-full py-20  rounded-tl-3xl rounded-tr-3xl text-white text-[15vw] sm:text-[15vw] md:text-[5vw] lg:text-[4.5vw] leading-none tracking-tighter font-semibold uppercase'>   eye opening  </h1>
         </div>
-        <div className='w-1/2'>
-
+        < div className='w-1/2'>
+        <h1 className='w-full py-20  rounded-tl-3xl rounded-tr-3xl text-white  sm:text-[5vw] md:text-[5vw] lg:text-[2vw] leading-none tracking-tighter font-semibold uppercase justify-between '> DevBrain Code Jam – where curious minds meet challenges, break barriers with code, and create innovative solutions that shape the future of technology </h1>
+        
         </div>
     </div>
   )
